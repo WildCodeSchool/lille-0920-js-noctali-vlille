323b744518b0019f886bd7027712c81b660a0857
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2020-10-28 10:05:12 +0100

    Modification du fichier App, , création du dossier Components et ajout du composant Map

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
-import './App.css';
+import React from "react";
+import MapLille from "./components/MapLille";
 
 function App() {
-  return (
-    <main className="container">
-      <div className="item">
-        <h1>Hello world !</h1>
-      </div>
-      <div className="item">
-        <p>Welcome to your fresh, lightweight, React App ! &#127752;</p>
-      </div>
-      <div className="item">
-        <p>
-          Start in the <code>App.jsx</code> component !
-        </p>
-      </div>
-    </main>
-  );
+  return <MapLille />;
 }
 
 export default App;
---
+++ src/components/MapLille.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import axios from "axios";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+
+class MapLille extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stations: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=251&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        console.log(data.records);
+        this.setState({
+          stations: data.records,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Lille</h1>
+        <Map center={[50.6365654, 3.0635282]} zoom={14}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {this.state.stations.map((station) => (
+            <Marker
+              key={station.fields.nom}
+              position={[
+                station.geometry.coordinates[1],
+                station.geometry.coordinates[0],
+              ]}
+            />
+          ))}
+        </Map>
+      </div>
+    );
+  }
+}
+
+export default MapLille;
